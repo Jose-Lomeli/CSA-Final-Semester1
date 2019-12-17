6bc7fd11e78f86652cd9228375147fcc24223a68
Jose-Lomeli <54290188+Jose-Lomeli@users.noreply.github.com>
2019-12-17 14:51:07 -0800

    Update game.java

--- game.java
+++ game.java
@@ -1,30 +1,31 @@
-public class game {
+public class game
+{
     private String plot;
 
-    public game() {
+    public game()
+    {
         this.plot = plot;
+    }//adn game
 
-    }
-
-    public game(String newPlot) {
+    public game(String newPlot)
+    {
         plot = newPlot;
-    }
+    }//end game
 
-    public String getPlot() {
+    public String getPlot()
+    {
         return plot;
-    }
+    }// end getplot
 
-    public void setPlot(String newSetPlot) {
+    public void setPlot(String newSetPlot)
+    {
         plot = newSetPlot;
-    }
-
+    }//end set plot
 
     public String toString()
     {
         String output = "";
         output += " " + plot;
-
-
         return output;
     }// end toString
 
